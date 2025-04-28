d65ca218ecfdcc0ed6dd8ef72301c75ad42f3644
RaiYan163 <raiyanashraf20@gmail.com>
2025-04-28 18:03:42 +0600

    updated render

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const careerRoutes = require('./routes/careerRoutes');
 
 const app = express();
 
-const allowedOrigins = ['http://localhost:3000', 'https://development.d89p9807lpw00.amplifyapp.com'];
+const allowedOrigins = ['http://localhost:3000', 'https://development.d3b7tf0u4wf70e.amplifyapp.com'];
 
 // Enable CORS
 app.use(cors({
